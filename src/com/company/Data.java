5386f9e436b87f1916a57b497531916bbd9e794e
Yifan-Zhang618 <61165679+Yifan-Zhang618@users.noreply.github.com>
2020-02-22 23:19:09 -0800

    data class change

--- src/com/company/Data.java
+++ src/com/company/Data.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Data {
     //Contains protected data structure of services
-    //may contain protected functions to write / read to files
+    //may contain protected functions to write / read to file
     //may contain protected or public data that is found in both memberData and providerData
-
+    protected String name;// Member/Povider name
+    protected int number[9];// Member/Povider number (9 digits)
+    protected String address;// Member/Povider street address
+    protected String city;// Member/Povider city
+    protected char state[2];// Member/Povider state (2 letters)
+    protected int zipCode[5];// Member/Povider (5 digits)
 }
