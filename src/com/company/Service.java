3d6a23d0d94d8933f6b4cb8c87c15a62b44f6a4a
Roman Bogushevich <50647859+romanbog@users.noreply.github.com>
2020-03-01 20:44:51 -0800

    Randomly updated .idea/misc.xml to make the code work on my machine :^> Removed Service.receiving_date_time; Added Service constructor from Scanner. Added Service.printAll Added memberData constructor from Scanner. -Constructor fills in all memberData fields -Constructor populates memberData.services Added memberData.printAll Added member/1.txt -example file for members -providers should look similar, but in another directory Added memberData main -creates a file object from member/1.txt -creates a scanner from the file object -Passes the scanner into memberData constructor -calls memberData.printAll

--- src/com/company/Service.java
+++ src/com/company/Service.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Service {
-    Date service_date;
-    Date receiving_date_time;
+    Date service_date; //contains date and time
+    //Date receiving_date_time;
     String member_name;
     String provider_name;
     String service_name;
@@ -16,5 +18,33 @@ public class Service {
 
     public Service(Scanner fileInput){
         //Populate Service class from scanner fileInput object
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        try {
+            service_date = formatter.parse(fileInput.nextLine());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        member_name = fileInput.nextLine();
+        provider_name = fileInput.nextLine();
+        service_name = fileInput.nextLine();
+        comments = fileInput.nextLine();
+        member_id = fileInput.nextInt();
+        service_id = fileInput.nextInt();
+        fee = fileInput.nextDouble();
+        //fileInput.nextLine();
+
+        //this.printAll();
+    }
+
+    public void printAll(){
+        System.out.println(service_date);
+        System.out.println(member_name);
+        System.out.println(provider_name);
+        System.out.println(service_name);
+        System.out.println(comments);
+        System.out.println(member_id);
+        System.out.println(service_id);
+        System.out.println(fee);
     }
 }
