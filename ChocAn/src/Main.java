3248601ef7f8ac1922d92872f799fe30f1c3e6bf
Yifan-Zhang618 <61165679+Yifan-Zhang618@users.noreply.github.com>
2020-03-05 23:43:07 -0800

    update

--- ChocAn/src/Main.java
+++ ChocAn/src/Main.java
@@ -1,25 +1,19 @@
-package com.company;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        dataController data = new dataController();
+       dataController data = new dataController();
         data.printMembers();
-        data.printServices();
+         data.printServices();
         data.printProviders();
         String memberFile="data/member.txt";
         String providerFile="data/provider.txt";
-        //      memberData m=new memberData();
+  //      memberData m=new memberData();
         //data.writeInfor(m,memberFile);
         //data.writeInfor(m,providerFile);
-        //       data.writeService(new Service(),"data/service.txt");
-
+ //       data.writeService(new Service(),"data/service.txt");
     }
 }
